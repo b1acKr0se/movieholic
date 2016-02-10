d8753df7dd05dc6a00cb91989bd080383499bbec
haint <haint.fit12@gmail.com>
2016-02-10 20:36:23 +0700

    Auto generated template

---
+++ app/src/androidTest/java/b1ackr0se/io/movieholic/ApplicationTest.java
@@ -0,0 +1,13 @@
+package b1ackr0se.io.movieholic;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/b1ackr0se/io/movieholic/MainActivity.java
@@ -0,0 +1,13 @@
+package b1ackr0se.io.movieholic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
